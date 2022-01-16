27bbadeabf1c334db8ff21c32d0a823135073a01
KarolinaKoza <97195524+Pluszka@users.noreply.github.com>
2022-01-16 19:05:36 +0000

    checking player answers

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from random import randint
+import os
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
 answer=randint(1,100)
 
 levels_options=['h','e']
 lvl=None
 
 while not lvl in levels_options:
+  clearConsole()
   lvl=input('Let\'s guess the number! \nPlease choose a level of difficulty "e" for easy or "h" for hard.').lower()
-  print(3)
-# if lvl==
+
+if lvl=='e':
+  attemps=5
+else:
+  attemps=10
+print(f'You have {attemps}, gl <3')
+
+for guess_attemps in range(attemps):
+  guess=int(input('Type your guess: '))
+  if guess==answer:
+    print('You guessed!')
+    break
+  elif guess<answer:
+    print('Too low.')
+  else:
+    print('Too high.')
+
+print(answer)
