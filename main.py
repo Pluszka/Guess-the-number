54b6de17aa58639dde7c6ba69f011d63bc8e661a
KarolinaKoza <97195524+Pluszka@users.noreply.github.com>
2022-01-16 19:26:34 +0000

    looped game + new file with art

--- main.py
+++ main.py
@@ -6,26 +6,33 @@ clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 answer=randint(1,100)
 
 levels_options=['h','e']
-lvl=None
 
-while not lvl in levels_options:
-  clearConsole()
-  lvl=input('Let\'s guess the number! \nPlease choose a level of difficulty "e" for easy or "h" for hard.').lower()
+def game():
 
-if lvl=='e':
-  attemps=5
-else:
-  attemps=10
-print(f'You have {attemps}, gl <3')
+  lvl=None
+  while not lvl in levels_options:
+    clearConsole()
+    lvl=input('Let\'s guess the number! \nPlease choose a level of difficulty "e" for easy or "h" for hard.').lower()
 
-for guess_attemps in range(attemps):
-  guess=int(input('Type your guess: '))
-  if guess==answer:
-    print('You guessed!')
-    break
-  elif guess<answer:
-    print('Too low.')
+  if lvl=='e':
+    attemps=5
   else:
-    print('Too high.')
+    attemps=10
+  print(f'You have {attemps}, gl <3')
 
-print(answer)
+  for guess_attemps in range(attemps):
+    guess=int(input('Type your guess: '))
+    if guess==answer:
+      print('You guessed!')
+      break
+    elif guess<answer:
+      print('Too low.')
+    else:
+      print('Too high.')
+
+  print(f'The answer was {answer}')
+  new_game=input('Will you play again? (Y for yes/ anything for no)').upper()
+  if new_game=="Y":
+    game()
+
+game()
